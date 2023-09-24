23c9041d32a00baf66513f4e026c2bab60c1dbc6
VuittonVU <145370052+VuittonVU@users.noreply.github.com>
2023-09-24 21:07:12 +0700

    Kuis 1

---
+++ kuis_1_A_Vuitton_063.pas
@@ -0,0 +1,14 @@
+program soal_tiga;
+var 
+nama,nim,kom,alamat:string;
+begin
+    writeln('Masukkan Nama Anda: ');
+    readln(nama);
+    writeln('Masukkan NIM Anda: ');
+    readln(nim);
+    writeln('Masukkan Kom Anda: ');
+    readln(kom);
+    writeln('Masukkan Alamat Anda: ');
+    readln(alamat);
+    writeln('Hallo, Nama saya ',nama,', NIM saya ',nim,', saya di kom ',kom,', alamat saya di ',alamat,'. Salam Kenal Semua!');
+end.
---
+++ kuis_1_B_Vuitton_063.pas
@@ -0,0 +1,14 @@
+program soal_empat;
+var 
+jalan,no,kota,kode:string;
+begin
+ writeln('Jalan :');
+ readln(jalan);
+  writeln('No :');
+ readln(no);
+  writeln('Kota :');
+ readln(kota);
+  writeln('Kode pos :');
+ readln(kode);
+ writeln('Alamat : Jalan ',jalan,' No.',no,' ',kota,' ',kode);
+ end.
